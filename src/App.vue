e86cef5986eff9f9315e1c540a8eee40a0c36c7b
my <256440961@qq.com>
2025-09-21 21:10:22 +0800

    代码提交

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+
+
+<template>
+  <div>
+    <h1>{{ people.name }}</h1>
+    <h1>{{ people.age }}</h1>
+    <h1>{{ re }}</h1>
+  </div>
+  <br>
+
+<div>
+  <input v-model="people.name">
+  <input type="button" @click="changeAge" value="改变年龄">
+</div>
+</template>
+
+<script setup>
+import { computed, ref,watch } from 'vue'
+const people=ref({name:'张三',age:20})
+function changeAge(){
+  people.value.age+=1
+}
+let re=computed({
+  get(){
+     return people.value.name+'age is'+people.value.age
+
+  }
+  ,set(value){
+    console.log('set得到的'+value)
+  }
+ 
+   
+ 
+})
+let stop=watch(people,(newValue,oldValue)=>{
+  console.log(newValue,oldValue)
+  if(newValue.age>30){
+    stop()
+  }
+
+},{deep:true})
+
+
+</script>
+
+
+
---
+++ src/components/Component1.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        图片
+        <img id="tt" src="../../public/bg.png" alt="本地图片" >
+         <p class="quote">这是一段引用文本</p>
+<!-- 渲染后效果："这是一段引用文本"（引号为红色） -->
+    </div>
+
+</template>
+<style>
+/* 在 .quote 元素内容前添加左引号，内容后添加右引号 */
+#tt {
+  width: 200px;
+  height: 100px;
+  padding: 10px;
+  border: 5px solid #000;
+  margin: 5px #f00;
+  /* 标准盒模型下：
+     总宽度 = 200 + 10*2 + 5*2 = 230px
+     总高度 = 100 + 10*2 + 5*2 = 130px */
+}
+</style>
